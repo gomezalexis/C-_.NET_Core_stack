35a46a9037a45ad6946373d05e4f4f8c7a29777c
Alexis Gomez <gomez.alexis@gmail.com>
2017-10-30 12:15:02 -0500

    First C# fundamentals work

---
+++ Fundamentals1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            for(int i = 1; i <= 255; i++){
+                Console.WriteLine(i);
+            }
+            
+            Console.WriteLine("The second excercise...");
+            
+            for(int i = 1; i <= 100; i++){
+                if(i % 3 == 0 && i % 5 != 0){
+                    System.Console.WriteLine(i + " - Fizz");
+                } else if(i % 5 == 0 && i % 3 != 0){
+                    System.Console.WriteLine(i + " - Buzz");
+                } else if(i % 5 == 0 && i % 3 == 0){
+                    System.Console.WriteLine(i + " - FizzBuzz");
+                }
+            }
+
+        }
+    }
+}
