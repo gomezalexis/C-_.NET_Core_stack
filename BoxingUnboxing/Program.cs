b867d85fbe17c5d78c6181b47187d79a0dfe0211
Alexis Gomez <gomez.alexis@gmail.com>
2017-10-30 17:01:07 -0500

    Boxing and Unboxing

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int sumOfNumbers = 0;
+            List<object> whatevers = new List<object>();
+
+            whatevers.Add(7);
+            whatevers.Add(28);
+            whatevers.Add(-1);
+            whatevers.Add(true);
+            whatevers.Add("chair");
+
+            foreach(object whatever in whatevers){
+                if(whatever is int){
+                    System.Console.WriteLine(whatever);
+                    sumOfNumbers += Convert.ToInt32(whatever);
+                }
+
+                if(whatever is string){
+                    System.Console.WriteLine(whatever);
+                }
+
+                if(whatever is bool){
+                    System.Console.WriteLine(whatever);
+                }
+            }
+
+            System.Console.WriteLine("The sum of numbers is: {0}", sumOfNumbers);
+
+
+
+        }
+
+        
+    }
+}
